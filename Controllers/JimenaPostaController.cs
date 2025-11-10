32ba53d4a401570964e1bd76999dea61981b5c8c
Your Name <you@example.com>
2025-11-10 20:19:51 -0300

    Api alumno

---
+++ Controllers/JimenaPostaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace APIporAlumno.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JimenaPostaController : Controller
+    {
+        public readonly HttpClient _httpClient;
+
+        public JimenaPostaController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7163/api/GabrielPosta");
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("\"Por aqui paso la posta de Jimena");
+                var data = await response.Content.ReadAsStringAsync();
+                return Ok(data);
+            }
+
+            return StatusCode((int)response.StatusCode, "Error fetching data");
+        }
+    }
+}
